439769c8cc526c06852843de68923c3c51426236
Mohammad Moradi <53098070+Fontex5@users.noreply.github.com>
2021-11-10 00:14:05 +0330

    Create Android App.java

---
+++ Android App.java
@@ -0,0 +1,130 @@
+package com.example.iotapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+    public String clientId;
+    public MqttAndroidClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Switch fanS = findViewById(R.id.FanSwitch);
+        fanS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(fanS.isChecked()){
+                    Send_req("ON");
+                }
+                else {Send_req("OFF");}
+            }
+        });
+    }
+    public void Send_req(String payload){
+        String topic = "fan/status";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connectButton(View view) {
+        clientId = MqttClient.generateClientId();
+		
+		//Enter the MQTT Server address instead of [MQTT SERVER]
+        client =
+                new MqttAndroidClient(MainActivity.this, "MQTT SERVER",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    //Log.d(TAG, "onSuccess");
+                    Toast.makeText(MainActivity.this, "Connected To MQTT Server", Toast.LENGTH_SHORT).show();
+                    Subscribe("room/temp");
+                    Subscribe("room/hum");
+                    client.setCallback(new MqttCallback() {
+                        @Override
+                        public void connectionLost(Throwable cause) {
+
+                        }
+
+                        @Override
+                        public void messageArrived(String topic, MqttMessage message) throws Exception {
+                            TextView temp , hum;
+                            temp = findViewById(R.id.TempText);
+                            hum = findViewById(R.id.HumText);
+                            if(topic.equals("room/temp"))
+                                temp.setText("Tempreture = "+message.toString());
+                            if(topic.equals("room/hum"))
+                                hum.setText("Humidity = " + message.toString());
+                        }
+
+                        @Override
+                        public void deliveryComplete(IMqttDeliveryToken token) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    //Log.d(TAG, "onFailure");
+                    Toast.makeText(MainActivity.this, "Failed to Connect", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Subscribe(String topic){
+        int qos = 2;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // The message was published
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
